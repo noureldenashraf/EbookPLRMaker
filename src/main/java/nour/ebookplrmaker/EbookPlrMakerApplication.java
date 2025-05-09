777655be270ebe0ef074da1c040bcfc402da27d8
Eng-Nour <n.ashraf956@gmail.com>
2025-05-09 04:26:53 +0300

    initalize the repo

---
+++ src/main/java/nour/ebookplrmaker/EbookPlrMakerApplication.java
@@ -0,0 +1,13 @@
+package nour.ebookplrmaker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EbookPlrMakerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EbookPlrMakerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/nour/ebookplrmaker/EbookPlrMakerApplicationTests.java
@@ -0,0 +1,13 @@
+package nour.ebookplrmaker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EbookPlrMakerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
